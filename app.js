8ecfda44fb6715ad8865f9af9340387dd8504386
Adam Bachman <adam.bachman@gmail.com>
2020-04-10 22:15:30 -0400

    initial commit of working bare bones p5.websocket server

---
+++ app.js
@@ -0,0 +1,56 @@
+import Websocket from "ws";
+import http from "http";
+import url from "url";
+import { v4 as uuid4 } from "uuid";
+
+import SocketCollection from "./lib/socket_collection";
+
+const server = http.createServer();
+const wss = new Websocket.Server({ clientTracking: false, noServer: true });
+
+const sketches = {};
+// const Sockets = new SocketCollection();
+
+server.on("upgrade", function (request, socket, head) {
+  // get path
+  const pathname = url.parse(request.url).pathname;
+
+  if (!sketches[pathname]) {
+    console.log("starting sketch at", pathname);
+    sketches[pathname] = {
+      sockets: new SocketCollection(),
+    };
+  }
+
+  wss.handleUpgrade(request, socket, head, function (ws) {
+    wss.emit("connection", ws, request);
+  });
+});
+
+wss.on("connection", function (ws, req) {
+  const uid = uuid4();
+  const pathname = url.parse(req.url).pathname;
+  const sketch = sketches[pathname];
+  if (!sketch) {
+    // unrecognized!
+    console.error("unrecognized path:", pathname, "closing session");
+    ws.close();
+    return;
+  }
+
+  const { sockets } = sketch;
+  sockets.addConnection(ws, req, uid);
+
+  ws.on("message", function (data) {
+    sockets.onMessage(data);
+  });
+
+  ws.on("close", function () {
+    sockets.removeConnection(uid);
+  });
+});
+
+// listen for requests :)
+const listener = server.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
