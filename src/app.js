9962550c442ed56198c638dcbf23e0a8e55efb0b
Adam Bachman <adam.bachman@gmail.com>
2020-04-18 16:13:16 -0400

    more aggressively allowing iframing

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import session from "express-session/";
 import url from "url";
 import path from "path";
 import exphbs from "express-handlebars";
+import frameguard from "frameguard";
 import { v4 as uuid4 } from "uuid";
 import morgan from "morgan";
 
@@ -22,8 +23,10 @@ const publicDir = path.resolve(__dirname, "../public");
 app.use(express.static(publicDir));
 app.use(sessionParser);
 app.use(morgan("combined"));
+app.use(frameguard({ action: "sameorigin" }));
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
+app.disable("x-powered-by");
 
 app.get("/", (req, res) => {
   res.render("home", { layout: "sketches" });
@@ -34,13 +37,6 @@ app.get("/sketch/:name", (req, res) => {
 });
 
 app.get("/embed/:name", (req, res) => {
-  res.header("X-Frame-Options", "SAMEORIGIN");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, X-Requested-With, Authorization, X-Frame-Options"
-  );
   res.render("embed", { layout: "embed", name: req.params["name"] });
 });
 
