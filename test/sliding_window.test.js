acde7562baeb77c17077bc1c04f27a019a95b8d9
Adam Bachman <adam.bachman@gmail.com>
2021-03-09 22:48:54 -0500

    switch to jest for testing

--- test/sliding_window.test.js
+++ test/sliding_window.test.js
@@ -1,9 +1,7 @@
-import { equal } from "assert";
-
 describe("Array", function () {
   describe("#indexOf()", function () {
     it("should return -1 when the value is not present", function () {
-      equal([1, 2, 3].indexOf(4), -1);
+      expect([1, 2, 3].indexOf(4)).toBe(-1);
     });
   });
 });
