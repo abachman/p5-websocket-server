7405b19115796084c23799afdb6373213f034b6a
Adam Bachman <13002+abachman@users.noreply.github.com>
2026-04-01 14:54:49 -0400

    scrambling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,10 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
+export default [
   eslintConfigPrettier,
   {
     ignores: ['dist/**', 'node_modules/**'],
   }
-);
+]
